80518dfe3a62d4054c43c611f61e5768687c9880
VAPortyanko <PortyankoWork@gmail.com>
2019-08-15 04:13:33 +0300

    Initial commit

---
+++ Testing/Testing/src/main/java/by/pva/testing/classesForTesting/Calculator.java
@@ -0,0 +1,9 @@
+package by.pva.testing.classesForTesting;
+
+public class Calculator {
+
+	public int add(int a, int b) {
+		return a + b;
+	}
+
+}
---
+++ Testing/Testing/src/test/java/by/pva/testing/calculatorTests/TestWithMetaAndComposedAnnotations.java
@@ -0,0 +1,25 @@
+package by.pva.testing.calculatorTests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import by.pva.testing.classesForTesting.Calculator;
+import by.pva.testing.customAnnotations.composedAnnotation.FastTest;
+import by.pva.testing.customAnnotations.metaAnnotation.Fast;
+
+class TestWithMetaAndComposedAnnotations {
+
+	private final Calculator calculator = new Calculator();
+
+	@Test
+	@Fast // Meta annotation.
+	void addition1() {
+		assertEquals(4, calculator.add(1, 1));
+	}
+	
+	@FastTest // Composed annotation.
+	void addition2() {
+		assertEquals(2, calculator.add(1, 1));
+	}
+}
