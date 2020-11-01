8e4ba5991f445df69f04d8e04a988aeafc699995
VPortyanko <PortyankoWork@gmail.com>
2020-11-01 15:33:58 +0300

    Add hibernate examples (Collection types - bags)

--- Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/types/value_types/collection_types/collectionsOfEntities/bags/TestUnidirectionalBag.java
+++ Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/types/value_types/collection_types/collectionsOfEntities/bags/TestUnidirectionalBag.java
@@ -45,7 +45,9 @@ class Person {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@OneToMany(cascade = CascadeType.ALL)
+	// The cascading mechanism allows you to propagate an entity state transition from a parent entity to its children.
+	// Once the parent entity is persisted, the child entities are going to be persisted as well.
+	@OneToMany(cascade = CascadeType.ALL) 
 	private List<Phone> phones = new ArrayList<>();
 	public Long getId() {
 		return id;
@@ -96,3 +98,34 @@ class Phone {
 	}
 
 }
+
+/*
+CREATE TABLE Person (
+    id BIGINT NOT NULL ,
+    PRIMARY KEY ( id )
+)
+
+CREATE TABLE Person_Phone (
+    Person_id BIGINT NOT NULL ,
+    phones_id BIGINT NOT NULL
+)
+
+CREATE TABLE Phone (
+    id BIGINT NOT NULL ,
+    number VARCHAR(255) ,
+    type VARCHAR(255) ,
+    PRIMARY KEY ( id )
+)
+
+ALTER TABLE Person_Phone
+ADD CONSTRAINT UK_9uhc5itwc9h5gcng944pcaslf
+UNIQUE (phones_id)
+
+ALTER TABLE Person_Phone
+ADD CONSTRAINT FKr38us2n8g5p9rj0b494sd3391
+FOREIGN KEY (phones_id) REFERENCES Phone
+
+ALTER TABLE Person_Phone
+ADD CONSTRAINT FK2ex4e4p7w1cj310kg2woisjl2
+FOREIGN KEY (Person_id) REFERENCES Person
+*/
