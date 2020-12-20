e9e89ce3cdd93c0fe48302c1ab5264da13b0a89a
VPortyanko <PortyankoWork@gmail.com>
2020-12-20 17:46:04 +0300

    * Project "Learning / Hibernate / Hibernate JBoss.org part_01":    + Add new example (Batching)    ! Small fixes

--- Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/_myUtils/MyUtils.java
+++ Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/_myUtils/MyUtils.java
@@ -5,6 +5,7 @@ import java.util.function.Consumer;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class MyUtils {
@@ -12,19 +13,32 @@ public class MyUtils {
 	@SuppressWarnings("rawtypes")
 	public static void doInHibernateWithDefaultPersistanceUnit(Consumer<EntityManager> code, Map properties) {
 
-		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("by.pva.hibernate.part01.basicWithTableAutoGeneration", properties);
-		
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		
-		code.accept(entityManager);
-		
-		entityManager.getTransaction().commit();
-		entityManager.close();
-		entityManagerFactory.close();
-		
+		EntityManagerFactory entityManagerFactory = Persistence
+				.createEntityManagerFactory("by.pva.hibernate.part01.basicWithTableAutoGeneration", properties);
+
+		EntityManager entityManager = null;
+		EntityTransaction txn = null;
+		try {
+			entityManager = entityManagerFactory.createEntityManager();
+
+			txn = entityManager.getTransaction();
+			txn.begin();
+
+			code.accept(entityManager);
+
+			txn.commit();
+		} catch (RuntimeException e) {
+			if (txn != null && txn.isActive())
+				txn.rollback();
+			throw e;
+		} finally {
+			if (entityManager != null) {
+				entityManager.close();
+			}
+			entityManagerFactory.close();
+		}
 	}
-	
+
 	public static void doInHibernateWithDefaultPersistanceUnit(Consumer<EntityManager> code) {
 		doInHibernateWithDefaultPersistanceUnit(code, null);
 	}
---
+++ Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/batching/TestBatching.java
@@ -0,0 +1,87 @@
+package by.pva.hibernate.part01.batching;
+
+import static by.pva.hibernate.part01._myUtils.MyUtils.doInHibernateWithDefaultPersistanceUnit;
+
+import java.util.Collections;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Query;
+import javax.persistence.Table;
+
+//To enable batch loging you need to add "BatchingBatch" logger to your logging provider.
+// org.hibernate.engine.jdbc.batch.internal.BatchingBatch
+// Than you will be able to see in the logs something like:
+// 2018-02-20 17:33:41.279 DEBUG 6280 --- [           main] o.h.e.jdbc.batch.internal.BatchingBatch  : Executing batch size: 20
+// Unless you see this message,batching is not working.
+// https://stackoverflow.com/questions/42002249/hibernate-how-to-verify-if-batch-insert-is-really-performed
+
+
+// Since version 5.2, Hibernate allows overriding the global JDBC batch size
+// given by the hibernate.jdbc.batch_size configuration property on a per Session basis:
+// entityManager.unwrap(Session.class)
+//              .setJdbcBatchSize(10);
+
+public class TestBatching {
+
+	public static void main(String[] args) {
+		
+		// In order to be optimal you have to define your jdbc.batch_size and your flushing param identical.
+		final int BATCH_SIZE = 20;
+		
+		Map<String, String> properties = Collections.singletonMap("hibernate.jdbc.batch_size", String.valueOf(BATCH_SIZE));
+		
+		doInHibernateWithDefaultPersistanceUnit(entityManager -> {
+			
+			Query query = entityManager.createQuery("delete from Person40");
+			query.executeUpdate();
+			
+			for (int i = 0; i < 100; i++ ) {
+				if ( i > 0 && i % BATCH_SIZE == 0 ) {
+					//flush a batch of inserts and release memory
+					entityManager.flush();
+					entityManager.clear();
+					// The flush method forces Hibernate to save person entities to the database
+					// but without batching it will be done in 20 conversations to the database.
+					// With batching it will be done only one conversation to the database.
+					// https://stackoverflow.com/questions/45670583/how-hibernate-batch-insert-works 
+				}
+
+				Person Person = new Person(i, String.format( "Person %d", i ));
+				entityManager.persist(Person);
+			}
+			
+		}, properties);
+
+	}
+}
+
+@Entity(name = "Person40")
+@Table(name = "Persons40")
+class Person {
+	
+	@Id
+	private long id;
+	private String name;
+
+	public Person() {
+	}
+	public Person(long id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+}
