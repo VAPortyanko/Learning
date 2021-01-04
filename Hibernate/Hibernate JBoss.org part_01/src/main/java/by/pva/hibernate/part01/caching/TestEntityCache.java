efc2aa2420c1e5bcfef79f19d000d21148231077
VAPortyanko <PortyankoWork@gmail.com>
2021-01-04 13:20:02 +0300

    * Project "Learning / Hibernate / Hibernate JBoss.org part_01":    ! Update the TestEntityCache.java example.    ! Update the Hibernate.docx file.

--- Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/caching/TestEntityCache.java
+++ Hibernate/Hibernate JBoss.org part_01/src/main/java/by/pva/hibernate/part01/caching/TestEntityCache.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Query;
 import javax.persistence.Table;
-import javax.persistence.Version;
 
 import org.hibernate.annotations.CacheConcurrencyStrategy;
 
@@ -17,6 +16,7 @@ import by.pva.hibernate.part01._myUtils.BaseTest;
 
 public class TestEntityCache extends BaseTest {
 
+	@SuppressWarnings("unused")
 	public static void main(String[] args) {
 
 		Map<String, String> properties = new HashMap<>();
@@ -30,33 +30,43 @@ public class TestEntityCache extends BaseTest {
 			Query query1 = entityManager.createQuery("Delete from Phone25");
 			query1.executeUpdate();
 
-			Phone phone = new Phone();
-			phone.setId(1L);
-			phone.setNumber("+37529 567-98-11");
-			entityManager.persist(phone);
+			Phone25 phone25_1 = new Phone25();
+			phone25_1.setId(1L);
+			phone25_1.setNumber("+37529 567-98-11");
+
+			entityManager.persist(phone25_1);
 
 			entityManager.flush();
 			entityManager.clear();
 
-			Phone phone1 = entityManager.find(Phone.class, 1L);
-			System.out.println(phone1);
+			Phone25 phone25_2 = entityManager.find(Phone25.class, 1L);
 			entityManager.flush();
 			entityManager.clear();
-			Phone phone2 = entityManager.find(Phone.class, 1L);
-			System.out.println(phone2);
-			entityManager.flush();
-			entityManager.clear();
-			Phone phone3 = entityManager.find(Phone.class, 1L);
-			System.out.println(phone3);
-			System.out.println("end session");
+			
+			Phone25 phone25_3 = entityManager.find(Phone25.class, 1L);
+
+			System.out.println("End session #1");
 
 		});
 
 		doInJPA(entityManager -> {
-			System.out.println("sdaf");
-			//compare references!
-			Phone phone1 = entityManager.find(Phone.class, 1L);
-			System.out.println(phone1);
+			Phone25 phone25_4 = entityManager.find(Phone25.class, 1L);
+			System.out.println("End session #2");
+		});
+		
+		doInJPA(entityManager -> {
+			Phone25 phone25_5 = entityManager.find(Phone25.class, 1L);
+			System.out.println("End session #3");
+		});
+		
+		doInJPA(entityManager -> {
+			Phone25 phone25_6 = entityManager.find(Phone25.class, 1L);
+			System.out.println("End session #4");
+		});
+		
+		doInJPA(entityManager -> {
+			Phone25 phone25_7 = entityManager.find(Phone25.class, 1L);
+			System.out.println("End session #5");
 		});
 		
 		entityManagerFactory.close();
@@ -64,68 +74,30 @@ public class TestEntityCache extends BaseTest {
 }
 
 // https://stackoverflow.com/questions/37697028/hibernate-ehcache-second-level-cache-miss-in-simple-example
-// But, before returning the entity, it is stored in first level cache also so that next invocation to load method for entity will return the entity from first level cache itself, and there will not be need to go to second level cache again.
+// But, before returning the entity, it is stored in first level cache also so that next invocation to load method for entity will return the entity
+// from first level cache itself, and there will not be need to go to second level cache again.
 // https://www.java67.com/2017/10/difference-between-first-level-and-second-level-cache-in-Hibernate.html
 
 @Cacheable
 @org.hibernate.annotations.Cache(usage = CacheConcurrencyStrategy.READ_WRITE) // change!
 @Entity(name = "Phone25")
 @Table(name = "Phones25")
-class Phone {
+class Phone25 {
 
 	@Id
 	private Long id;
 	private String number;
 
-//	@ManyToOne
-//	private Person person;
-	@Version
-//  private int version;
-
 	public Long getId() {
 		return id;
 	}
-
 	public void setId(Long id) {
 		this.id = id;
 	}
-
 	public String getNumber() {
 		return number;
 	}
-
 	public void setNumber(String number) {
 		this.number = number;
 	}
-	// public Person getPerson() {
-//		return person;
-//	}
-//	public void setPerson(Person person) {
-//		this.person = person;
-//	}
-//	public int getVersion() {
-//		return version;
-//	}
-
 }
-
-//@Entity(name = "Person41")
-//@Table(name = "Persons41")
-//class Person{
-//	@Id
-//	private long id;
-//	private String name;
-//	public long getId() {
-//		return id;
-//	}
-//	public void setId(long id) {
-//		this.id = id;
-//	}
-//	public String getName() {
-//		return name;
-//	}
-//	public void setName(String name) {
-//		this.name = name;
-//	}
-//	
-//}
