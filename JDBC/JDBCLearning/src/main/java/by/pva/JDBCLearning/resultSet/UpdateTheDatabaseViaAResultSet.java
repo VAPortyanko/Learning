a06b642a0869f1d37015fafad2a6339bc3908ffb
VAPortyanko <PortyankoWork@gmail.com>
2021-06-21 15:04:44 +0300

    Project "Learning / JDBC":    ! Update the resultSet/UpdateTheDatabaseViaAResultSet.java example.

--- JDBC/JDBCLearning/src/main/java/by/pva/JDBCLearning/resultSet/UpdateTheDatabaseViaAResultSet.java
+++ JDBC/JDBCLearning/src/main/java/by/pva/JDBCLearning/resultSet/UpdateTheDatabaseViaAResultSet.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class UpdateTheDatabaseViaAResultSet {
 
-	private final static String DATABASE_NAME = "javastudy";
+	private final static String DATABASE_NAME = "jdbc";
 	private final static String TIMEZONE = "Europe/Minsk";
 	private final static String CONNECTION_URL = "jdbc:mysql://localhost:3306/" + DATABASE_NAME	+ "?useLegacyDatetimeCode=false&serverTimezone=" + TIMEZONE;
 	private final static String USER_NAME = "root";
@@ -19,18 +19,33 @@ public class UpdateTheDatabaseViaAResultSet {
 		try (Connection con = DriverManager.getConnection(CONNECTION_URL, USER_NAME, PASSWORD);
 			 Statement stmt = con.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);) {
 
-			ResultSet uprs = stmt.executeQuery("SELECT * FROM FORTEST");
+			ResultSet uprs = stmt.executeQuery("select * from contact");
 
+			System.out.println("**************************************************");
+			
 			while (uprs.next()) {
 				
-				int age = uprs.getInt("AGE");
-				uprs.updateInt("AGE", age + 1);
+				int id = uprs.getInt("id");
+				String fn = uprs.getString("first_name");
+				String ln = uprs.getString("last_name");
+				String bd = uprs.getString("birth_date");
+				int version = uprs.getInt("version");
+				
+				System.out.println("Id:" + id);
+				System.out.println("First name:" + fn);
+				System.out.println("Last name:" + ln);
+				System.out.println("Birth date:" + bd);
+				System.out.println("Version:" + version);
+				System.out.println("**************************************************");
+				
+				uprs.updateInt("version", version + 1);
 				uprs.updateRow();
 			}
-
+			
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
 	}
 }
+
 
