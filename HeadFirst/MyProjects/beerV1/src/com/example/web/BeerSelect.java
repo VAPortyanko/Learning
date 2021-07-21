a9d3322dbdb5e87eab2fbf4fa224a619e617f2b0
VPortyanko <PortyankoWork@gmail.com>
2021-07-21 23:06:17 +0300

    Project "HeadFirst/MyProjects/":    + Add the beerV1 project.

---
+++ HeadFirst/MyProjects/beerV1/src/com/example/web/BeerSelect.java
@@ -0,0 +1,24 @@
+package com.example.web;
+
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import java.io.*;
+
+@SuppressWarnings("serial")
+public class BeerSelect extends HttpServlet {
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		String c = request.getParameter("color");
+		
+		PrintWriter out = response.getWriter();
+		out.println("Beer Selection Advice<br>");
+		out.println("<br>Got beer color " + c);
+		out.println("<br>");
+		out.println("<a href=\"index.html\">Beer Advisor</a>");
+
+	}
+}
