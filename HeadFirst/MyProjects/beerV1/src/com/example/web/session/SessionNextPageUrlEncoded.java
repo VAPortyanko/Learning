4c6eff6dcc7ac1663a3393a07e14a0a9ed98c5b8
VAPortyanko <PortyankoWork@gmail.com>
2021-08-06 08:42:59 +0300

    Project "HeadFirst/MyProjects/":    ! Update the HeadFirst/Docs/Web app structure.docx file.    + Add a session via URL rewriting example.

---
+++ HeadFirst/MyProjects/beerV1/src/com/example/web/session/SessionNextPageUrlEncoded.java
@@ -0,0 +1,31 @@
+package com.example.web.session;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+@SuppressWarnings("serial")
+public class SessionNextPageUrlEncoded extends HttpServlet{
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+
+		HttpSession session = request.getSession();
+		
+		PrintWriter out = response.getWriter();
+		out.println("Session status: ");
+		out.println(session.isNew()?"new":"old");
+		out.println("<br>");
+		out.println("<br>");
+		out.println("<a href=\"" + response.encodeURL("testSessionURLRewriting") +"\">The session page (test session that use URL Rewriting)</a>");
+		
+	}
+
+}
