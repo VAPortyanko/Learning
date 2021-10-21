a22308b10875bda1fa8d717bc7ec0788dc0f3bf9
VAPortyanko <PortyankoWork@gmail.com>
2021-10-21 17:11:09 +0300

    project "SpringIO":    ! Update the example (Annotation based configuration - @Autowired).

--- Spring/SpringIO/src/main/java/by/pva/springio/annotationbasedconfiguration/autowired/AutowiredExample.java
+++ Spring/SpringIO/src/main/java/by/pva/springio/annotationbasedconfiguration/autowired/AutowiredExample.java
@@ -8,7 +8,6 @@ public class AutowiredExample {
 	public static void main(String[] args) {
 		
 		ApplicationContext context = new ClassPathXmlApplicationContext("AutowiredExample.xml");
-		
 		MainBean bean = (MainBean) context.getBean("mainBean");
 		System.out.println(bean.getDependency1().getName());
 		
--- Spring/SpringIO/src/main/java/by/pva/springio/annotationbasedconfiguration/autowired/MainBean.java
+++ Spring/SpringIO/src/main/java/by/pva/springio/annotationbasedconfiguration/autowired/MainBean.java
@@ -6,6 +6,11 @@ public class MainBean {
 	
 	private final DependencyBean dependency1;
 	
+	// As of Spring Framework 4.3, an @Autowired annotation on such a constructor is no longer necessary
+	// if the target bean defines only one constructor to begin with. However,
+	// if several constructors are available and there is no primary/default constructor,
+	// at least one of the constructors must be annotated with @Autowired in order to
+	// instruct the container which one to use.
 	@Autowired
 	public MainBean(DependencyBean dependency1) {
 		this.dependency1 = dependency1;
